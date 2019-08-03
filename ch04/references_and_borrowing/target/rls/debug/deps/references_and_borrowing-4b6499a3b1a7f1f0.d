941dc2c5aa86cc305d6a44e6d8fc4e89abcc4e68
Stefan Einarsson <stefane81@gmail.com>
2019-08-03 23:36:43 +0200

    ch04-01 complete

---
+++ ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-4b6499a3b1a7f1f0.d
@@ -0,0 +1,5 @@
+/home/stefane81/code/dev/rust/book/ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-4b6499a3b1a7f1f0.rmeta: src/main.rs
+
+/home/stefane81/code/dev/rust/book/ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-4b6499a3b1a7f1f0.d: src/main.rs
+
+src/main.rs:
---
+++ ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-7f60686838726ced.d
@@ -0,0 +1,5 @@
+/home/stefane81/code/dev/rust/book/ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-7f60686838726ced.rmeta: src/main.rs
+
+/home/stefane81/code/dev/rust/book/ch04/references_and_borrowing/target/rls/debug/deps/references_and_borrowing-7f60686838726ced.d: src/main.rs
+
+src/main.rs:
