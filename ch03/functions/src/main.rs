6fa989256cb05293b2888f7ebf343af255ce600f
Stefan Einarsson <stefane81@gmail.com>
2019-08-03 14:55:19 +0200

    ch03 functions complete

--- ch03/functions/src/main.rs
+++ ch03/functions/src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5,6);
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x+1
+    };
+
+    println!("x: {}, y: {}",x,y);
+    
+    println!("expression: {}, function_Return: {}",expression_five(),func_five());
 }
 
-fn another_function() {
-    println!("Another func");
+fn another_function(x: i32, y:i32) {
+    println!("Another func {},{}",x,y);
+}
+
+fn expression_five()->i32 {
+    5
+}
+
+fn func_five()-> i32{
+    let x:i32=5;
+    return 5;
 }
