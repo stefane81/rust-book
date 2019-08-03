761c7b199dda8a7ab7bc8a2bfd6f4dc6f8619e20
Stefan Einarsson <stefane81@gmail.com>
2019-08-03 19:21:19 +0200

    ch03 complete

--- ch03/functions/src/main.rs
+++ ch03/functions/src/main.rs
@@ -3,6 +3,18 @@ fn main() {
 
     another_function(5,6);
 
+    expressives();
+    loop_loop();
+    while_loop();
+    for_loop();
+
+}
+
+fn another_function(x: i32, y:i32) {
+    println!("Another func {},{}",x,y);
+}
+
+fn expressives() {
     let x = 5;
 
     let y = {
@@ -15,10 +27,6 @@ fn main() {
     println!("expression: {}, function_Return: {}",expression_five(),func_five());
 }
 
-fn another_function(x: i32, y:i32) {
-    println!("Another func {},{}",x,y);
-}
-
 fn expression_five()->i32 {
     5
 }
@@ -26,4 +34,44 @@ fn expression_five()->i32 {
 fn func_five()-> i32{
     let x:i32=5;
     return 5;
+}
+
+fn loop_loop(){
+    // Returning Values from Loops
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn while_loop(){
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!",number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF");
+}
+
+fn for_loop(){
+    let a = [10,20,30,40,50];
+
+    for element in a.iter(){
+        println!("{}",element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
