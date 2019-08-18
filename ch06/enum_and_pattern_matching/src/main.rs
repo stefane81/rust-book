659794e16e9e55096543230d93eda200d98d3cb9
Stefan Einarsson <stefane81@gmail.com>
2019-08-18 12:57:42 +0200

    ch06-01 complete

--- ch06/enum_and_pattern_matching/src/main.rs
+++ ch06/enum_and_pattern_matching/src/main.rs
@@ -14,8 +14,26 @@
 // }
 
 enum IpAddr {
-    V4(u8,u8,u8,u8),
+    V4(u8, u8, u8, u8),
     V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self){
+
+    }
+}
+
+enum Option<T> {
+    Some(T),
+    None,
 }
 
 fn main() {
@@ -33,8 +51,16 @@ fn main() {
     // };
 
     // let home = IpAddr::V4(String::from("127.0.0.1"));
-    let home = IpAddr::V4(127,0,0,1);
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let m =Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = Option::None;
 }
 
 // fn route(ip_kind: IpAddrKind) {}
