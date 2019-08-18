37bc9172d9eb850f40f812ce982a7b0a963ad20e
Stefan Einarsson <stefane81@gmail.com>
2019-08-18 15:47:21 +0200

    ch06-02 complete

--- ch06/match_flow/src/main.rs
+++ ch06/match_flow/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[derive(Debug)] 
     enum UsState {
         Alabama,
         Alaska,
@@ -25,4 +26,16 @@ fn main() {
             },
         }
     }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+
+let five = Some(5);
+let six = plus_one(five);
+let none = plus_one(None);
 }
